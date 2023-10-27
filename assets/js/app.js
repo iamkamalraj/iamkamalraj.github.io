42066c8c79d85af587b41b19b7fcee203baee021
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2023-10-27 23:38:36 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,7 +120,7 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" >When u feel like qutting, think about why you started.</p>
+      <p class="tag" >I am a dedicated individual passionate about consistent learning and coding in Java, with a focus on UI/UX design. Currently pursuing a Bachelor of Engineering in Computer Science and Engineering, my goal is to continuously enhance my technical skills for innovative organizations. My diverse academic background, which includes a lateral entry into BE in CSE, a Post Diploma in Plastic Mold Design, and a Diploma in Mechanical Engineering, showcases my adaptability and well-rounded skill set. Proficient in Java, UI/UX, and Blockchain, I have also excelled in graphic design, receiving awards and recognition for my work. With certifications in Enterprise Java with Spring Boot and Foundations of User Experience (UX) Design, I am well-prepared to collaborate effectively in diverse environments. I am enthusiastic about applying my skills and passion to make a meaningful impact in any dynamic work setting.</p>
     </div>
 
     
