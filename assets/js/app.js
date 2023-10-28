85ca67dd8b4cac1bc961fd76337f049c9b81b4df
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2023-10-28 22:02:17 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,7 +120,7 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" >A dedicated Java coder and UI/UX enthusiast with a versatile academic background in Computer Science and Engineering, Post Diploma in  Plastic Mold Design, and Diploma in Mechanical Engineering. Proficient in Java, UI/UX, and Blockchain, complemented by awards in graphic design. Currently learning Enterprise Java with Spring Boot and Foundations of UX Design, ready to excel in diverse collaborations. Eager to leverage my skills and passion for innovative impact in any dynamic work environment.</p>
+      <p class="tag" >When u feel like qutting, think about why you started.</p>
     </div>
 
     
