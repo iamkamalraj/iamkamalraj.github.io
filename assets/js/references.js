552c6c96a3cdc4148c194a11560044450419cd91
KAMAL RAJ P <kamalrajp.cse@gmail.com>
2024-01-11 01:16:55 +0530

    Update references.js

--- assets/js/references.js
+++ assets/js/references.js
@@ -1,34 +1,36 @@
-/*Reference Details Table*/
-// Changes made 10/1/2021:
-// Added reviewphoto1 - reviewphoto5 in assets/references-page
-// Changed names and descriptions of references
 const referenceTable = document.querySelector(".pain");
 
 const references = [
-  {
-    sl1: 1,
-        name1 : "Overall Designer",
-        designation1 : "Issued by ELOQUENCE'23, 6th National Level Symposium",
-        image1 : "assets/images/references-page/2.jpg",
-        message1 : "Overall design including Logo, Banners, Poster, Pamphlets, ID Cards, Certificates, Registration Cards, Invitations and Additional UI Design Elements for ELOQUENCE'23 Webiste which was organised by CSE department associated with C. Abdul Hakeem College of Engineering and Technology",
-
-        
-
-        sl1: 2,
-        name2 : "CODING and DEBUGGING",
-        designation2 : "Issued by TECHNO RENDEZVOUS'23", 
-        image2: "assets/images/references-page/3.png",
-        message2 : "Awarded Cash Prize in TECHNO RENDEZVOUS'23, A National Level Technical Symposium conducted by the Department Master of Computer Applications by C Abdul Hakeem College of Engineering and Technology",
-
-    
-        sl4: 4,
-        name4 : "Logo Design & Development",
-        designation4 : "Issued by Managing Director of Karthigeya Group Associated with Karthigeya Moulds & Dies Private Limited", 
-        image4 : "assets/images/references-page/led.jpg",
-        message4 : "Awarded Cash Prize and Incentive for the Logo Design and Development and special support in Graphic Design to Ledovec International Pvt Ltd.",
-         
+    {
+        sl: 1,
+        name: "SIH'23-Finalist (Smart India Hackathon 2023)",
+        designation: "Issued by ISRO for SIH 1521 - Development of Explainable AI (XAI) based model for prediction of heavy /high impact rain events using satellite data",
+        image: "assets/images/references-page/4.png",
+        message: "The final output is delivered through a user-friendly web application, showcasing model accuracy and providing explanations for the generated predictions. This marks the achievement of my goals for SIH'23 Final.",
+    },
+    {
+        sl: 3,
+        name: "Overall Designer",
+        designation: "Issued by ELOQUENCE'23, 6th National Level Symposium",
+        image: "assets/images/references-page/2.jpg",
+        message: "Overall design including Logo, Banners, Poster, Pamphlets, ID Cards, Certificates, Registration Cards, Invitations and Additional UI Design Elements for ELOQUENCE'23 Webiste which was organised by CSE department associated with C. Abdul Hakeem College of Engineering and Technology",
+    },
+    {
+        sl: 4,
+        name: "CODING and DEBUGGING",
+        designation: "Issued by TECHNO RENDEZVOUS'23",
+        image: "assets/images/references-page/3.png",
+        message: "Awarded Cash Prize in TECHNO RENDEZVOUS'23, A National Level Technical Symposium conducted by the Department Master of Computer Applications by C Abdul Hakeem College of Engineering and Technology",
+    },
+        {
+        sl: 2,
+        name: "Logo Design & Development",
+        designation: "Issued by Managing Director of Karthigeya Group Associated with Karthigeya Moulds & Dies Private Limited",
+        image: "assets/images/references-page/led.jpg",
+        message: "Awarded Cash Prize and Incentive for the Logo Design and Development and special support in Graphic Design to Ledovec International Pvt Ltd.",
     },
 
+  
 ];
 
 AOS.init();
@@ -37,76 +39,48 @@ const fillData = () => {
 
   references.forEach(
     ({
-      sl1,
-      image1,
-      name1,
-      designation1,
-      message1,
-      absbox_for_linkedin1,
-      sl2,
-      image2,
-      name2,
-      designation2,
-      message2,
-      absbox_for_linkedin2,
-      sl4,
-      image4,
-      name4,
-      designation4,
-      message4,
-      absbox_for_linkedin4,
-    }) =>
-      (output += `<tr data-aos="zoom-in-left"> 
-                <td class="imgCol"><img src="${image1}" class="rImg"></td>
-                <td class = "referenceTitleName">
-                    <div>
-                        <span class="imgResponsive">
-                            <img src="${image1}" class="imgRes">
-                        </span>
-                    </div>
-                    <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
-                    <a href="#0" class="paperTitle"> ${name1} </a> 
-                    <div> ${designation1} </div> <div class="rConferences">  
-                        <div class="referenceY">${message1}</div>
-                    </div>  
-                </td>
-            </tr> 
-            
-            <tr data-aos="zoom-in-left"> 
-                       
-                        <td class = "referenceTitleName">
-                            <div>
-                                <span class="imgResponsive">
-                                    <img src="${image2}" class="imgRes">
-                                </span>
-                            </div>
-                            <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
-                            <a href="#0" class="paperTitle">${name2} </a> 
-                            <div> ${designation2} </div> <div class="rConferences">
-                                <div class="referenceY">${message2}</div>
-                            </div>
-                
-    
-                        </td>
-                        <td class="imgCol"><img src="${image2}" class="rImg1"></td>
-
-                        <tr data-aos="zoom-in-left"> 
-                <td class="imgCol"><img src="${image4}" class="rImg"></td>
-                <td class = "referenceTitleName">
-                    <div>
-                        <span class="imgResponsive">
-                            <img src="${image4}" class="imgRes">
-                        </span>
-                    </div>
-                    <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
-                    <a href="#0" class="paperTitle"> ${name4} </a> 
-                    <div> ${designation4} </div> <div class="rConferences">  
-                        <div class="referenceY">${message4}</div>
-                    </div>  
-                </td>
-            </tr> 
-                    </tr>`)
+      sl,
+      image,
+      name,
+      designation,
+      message,
+    }) => {
+      if (sl === 1 || sl === 4) {
+        output += `<tr data-aos="zoom-in-left"> 
+                    <td class="imgCol"><img src="${image}" class="rImg"></td>
+                    <td class="referenceTitleName">
+                        <div>
+                            <span class="imgResponsive">
+                                <img src="${image}" class="imgRes">
+                            </span>
+                        </div>
+                        <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
+                        <a href="#0" class="paperTitle"> ${name} </a> 
+                        <div> ${designation} </div> <div class="rConferences">  
+                            <div class="referenceY">${message}</div>
+                        </div>  
+                    </td>
+                </tr>`;
+      } else {
+        output += `<tr data-aos="zoom-in-left"> 
+                    <td class="referenceTitleName">
+                        <div>
+                            <span class="imgResponsive">
+                                <img src="${image}" class="imgRes">
+                            </span>
+                        </div>
+                        <img src="https://img.icons8.com/fluency/48/000000/quote-left.png"/>
+                        <a href="#0" class="paperTitle"> ${name} </a> 
+                        <div> ${designation} </div> <div class="rConferences">  
+                            <div class="referenceY">${message}</div>
+                        </div>  
+                    </td>
+                    <td class="imgCol"><img src="${image}" class="rImg"></td>
+                </tr>`;
+      }
+    }
   );
   referenceTable.innerHTML = output;
 };
+
 document.addEventListener("DOMContentLoaded", fillData);
